8ba873ef4226de732db9b80c31478007a0b8363b
Anton <akuznetsov@catalystitservices.com>
2015-10-07 10:35:38 -0400

    first commit

---
+++ src/main/java/com/AD2015/americanDream/controller/HomeController.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package com.AD2015.americanDream.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+/**
+ * 
+ * @author aKuznetsov
+ *
+ */
+@Controller
+public class HomeController {
+	
+	
+	/**
+	 * @return
+	 */
+	@RequestMapping(value="/", method=RequestMethod.GET)
+	public String main(){
+		return "index";
+	}
+}
+
