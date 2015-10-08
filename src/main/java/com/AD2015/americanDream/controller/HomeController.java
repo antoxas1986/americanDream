d472688ff510e4b56a637a753586207d56564025
Anton Kuznetsov <kuznetsov74@gmail.com>
2015-10-07 23:28:43 -0400

    Signed-off-by: Anton Kuznetsov <kuznetsov74@gmail.com>

--- src/main/java/com/AD2015/americanDream/controller/HomeController.java
+++ src/main/java/com/AD2015/americanDream/controller/HomeController.java
@@ -3,6 +3,7 @@
  */
 package com.AD2015.americanDream.controller;
 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
