287f69c1348e91e8fd47da5a836dd6eaf9ad5512
Anton Kuznetsov <kuznetsov74@gmail.com>
2015-10-07 23:53:53 -0400

    Signed-off-by: Anton Kuznetsov <kuznetsov74@gmail.com>

--- bin/src/main/webapp/resources/components/index/indexController.js
+++
@@ -1,9 +0,0 @@
-(function() {
-	angular.module('controllers').controller('indexController',
-			[ '$rootScope', '$scope', 'indexService',
-			  function($rootScope, $scope, indexService) {
-				$scope.start = function(){
-					alert("Some start code");
-				}
-			} ]);
-}());
