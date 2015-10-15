4aa59f2be453d52a6dc76fd9b8fa490d31357018
Anton Kuznetsov <kuznetsov74@gmail.com>
2015-10-14 22:06:22 -0400

    front end design
    Signed-off-by: Anton Kuznetsov <kuznetsov74@gmail.com>

---
+++ src/main/java/com/AD2015/americanDream/dao/Impl/HeroDaoImpl.java
@@ -0,0 +1,26 @@
+package com.AD2015.americanDream.dao.Impl;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import com.AD2015.americanDream.daos.HeroDao;
+import com.AD2015.americanDream.models.Hero;
+
+public class HeroDaoImpl implements HeroDao {
+
+	public static final String UPDATE_HERO = "UPDATE hero SET name = ?,age =?, gender = ?, experiance = ?,money =?"
+			+ "strength=?,level=?,happieness=? WHERE name=?";
+
+	private JdbcTemplate jdbcTemplate;
+
+	public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+	}
+
+	@Override
+	public void updateHero(Hero hero) {
+
+		jdbcTemplate.update(UPDATE_HERO, hero.getName(), hero.getAge(), hero.getGender(), hero.getExperiance(),
+				hero.getMoney(), hero.getStrength(), hero.getLevel(), hero.getHappieness(), hero.getName());
+	}
+
+}
--- src/main/webapp/resources/components/index/indexService.js
+++ src/main/webapp/resources/components/index/indexService.js
@@ -2,9 +2,9 @@
 	angular.module('services')
 	.factory('indexService', function($resource, $rootScope){
 		return {
-			getHeroNames: $resource('/heroNames',{},{
-				'get':{method:'GET',isArray:true}
-			})
+//			getHeroNames: $resource('/',{},{
+//				'get':{method:'GET',isArray:true}
+//			})
 		}
 	})
 	
