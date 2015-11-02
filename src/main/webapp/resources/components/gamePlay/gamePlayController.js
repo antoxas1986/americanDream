254c1fc74939de280b77f362079339cee4f2c639
Dima <Dima@MyPc>
2015-11-01 21:09:14 -0500

    Added functionality to food menu select in gamePlay js and html

--- src/main/webapp/resources/components/gamePlay/gamePlayController.js
+++ src/main/webapp/resources/components/gamePlay/gamePlayController.js
@@ -8,23 +8,46 @@ angular
 						'gamePlayService',
 						'$timeout',
 						function($rootScope, $scope, gamePlayService, $timeout) {
+							
 
 							$scope.work = function() {
 
 							}
 							$scope.eat = function() {
-
+								document.getElementById('selectFood').style.display = "";
 								var foodSelect = angular.element(document
 										.querySelector('#selectFood'));
 								foodSelect.empty();
-								foodSelect.append('<select id="select">' 
+								foodSelect.append('<select id="select" onchange ="eatThis()">'
+										    + '<option value="apple">Food Menu</option>'
 											+ '<option value="apple">Apple</option>'
 											+ '<option value="soup">Soup</option>'
-											+ '<option value="burito">Burito</option>'
+											+ '<option value="burrito">Burrito</option>'
+											+ '<option value="cancel" style ="color:red">Cancel</option>'
 											+ '</select>'
-											);
-								
+											);	
 							};
+							    
+							    eatThis = function(){
+								var item = document.getElementById("select").value;
+								if (item == "apple" && $scope.hero.money >= 1){
+									document.getElementById('selectFood').style.display = "none";
+									$scope.hero.money -= 1;
+									$scope.hero.strength += 1;
+								}else if(item == "soup" && $scope.hero.money >=5){
+									document.getElementById('selectFood').style.display = "none";
+									$scope.hero.money -= 5;
+									$scope.hero.strength += 2;
+								}else if(item == "burrito" && $scope.hero.money >=10){
+									document.getElementById('selectFood').style.display = "none";
+									$scope.hero.money -= 10;
+									$scope.hero.strength += 3;
+								}else{
+									document.getElementById('selectFood').style.display = "none";
+							    }
+							    };
+						
+							
 							$scope.rest = function() {
 								$scope.hero.strength += 3;
 								document.getElementById("rest").disabled = true;
